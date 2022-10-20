3086784c1a177708bc2e7dbb3c42f331832d7643
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-10-19 21:56:14 -0700

    Added references to the buttons and score field. Added on click listener to display a message when clicking on either the left or right button.

--- app/src/main/java/com/example/guessinggame/MainActivity.kt
+++ app/src/main/java/com/example/guessinggame/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.guessinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get references to the buttons
+        val buttonLeft = findViewById<Button>(R.id.buttonLeft)
+        val buttonRight = findViewById<Button>(R.id.buttonRight)
+
+        // Get references to score field
+        val scoreViewText = findViewById<TextView>(R.id.scoreView)
+
+        // choose numbers to display on buttons
+        var  leftNum = Random.nextInt(0,10)
+        var  rightNum = Random.nextInt(0,10)
+
+        buttonLeft.setText(leftNum.toString())
+        buttonRight.setText(rightNum.toString())
+
+        buttonLeft.setOnClickListener{
+            Toast.makeText(this@MainActivity, "You clicked Left", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonRight.setOnClickListener{
+            Toast.makeText(this@MainActivity, "You clicked Right", Toast.LENGTH_SHORT).show()
+        }
     }
 }
